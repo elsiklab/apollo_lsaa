43e2d8e348f0d520c94cf617735cf79ec0c5b3f3
Colin <colin.diesh@gmail.com>
2016-07-11 11:45:51 -0500

    Split EditScaffoldsController and AddFastaController

--- grails-app/controllers/org/elsiklab/AddFastaController.groovy
+++ grails-app/controllers/org/elsiklab/AddFastaController.groovy
@@ -13,58 +13,9 @@ import org.bbop.apollo.Sequence
 import static org.springframework.http.HttpStatus.*
 
 
-class EditScaffoldsController {
+class AddFastaController {
 
-    def index() {
-        def text = new File("out.yaml").text
-        try {
-            def reference = new File("output.fasta").text
-            def ret = Yaml.load(new File("out.yaml"))
-            render view: "index", model: [yaml: text, reference: reference]
-        }
-        catch(YamlException e) {
-            e.printStackTrace()
-            render view: "index", model: [yaml: text, flash: [message: "Error parsing YAML"]]
-        }
-        catch(FileNotFoundException e) {
-            e.printStackTrace()
-            render view: "index", model: [yaml: text]
-        }
-    }
-
-    def editScaffold() {
-        new File("out.yaml").withWriter { out ->
-            out.write params.scaffoldEditor
-        }
-        redirect(action: "index")
-    }
-
-    def generateScaffolds() {
-        new File("temp.fa").withWriter { temp ->
-            EditScaffold.getAll().each { it ->
-                new File(it.filename).withReader { input ->
-                    temp << input
-                }
-            }
-
-            new File("out.fa").withWriter { out ->
-                ("scaffolder sequence out.yaml temp.fa").execute().waitForProcessOutput(out, System.err)
-            }
-        }
-        redirect(action: "downloadFasta")
-    }
-
-    def downloadFasta() {
-        log.debug "downloading"
-        new File("out.fa").withReader { stream ->
-            response.setHeader "Content-disposition", "attachment;filename=output.fa"
-            response.contentType = 'application/octet-stream'
-            response.outputStream << stream
-            response.outputStream.flush()
-        }
-    }
-
-    def addFasta(Integer max) {
+    def index(Integer max) {
         if(params.addFasta) {
             def f = File.createTempFile("fasta", null, null)
             f.withWriter { out ->
@@ -129,33 +80,4 @@ class EditScaffoldsController {
             '*' { render status: NOT_FOUND }
         }
     }
-
-    def createReversal(String sequence, Integer start, Integer end, String description) {
-        String name = UUID.randomUUID()
-        System.err.println sequence
-        Sequence s = Sequence.findByName(sequence)
-        System.err.println s
- 
-        AlternativeLoci altloci = new AlternativeLoci(
-            name: name,
-            uniqueName: name,
-            description: description ?: "",
-            filename: "blah"
-        ).save(flush: true,failOnError: true)
-        System.err.println altloci
- 
-        FeatureLocation featureLoc = new FeatureLocation(
-            fmin: start,
-            fmax: end,
-            feature: altloci,
-            sequence: s
-        ).save(flush:true)
-        System.err.println featureLoc
-        altloci.addToFeatureLocations(featureLoc)
-        render ([success: true] as JSON)
-    }
-    def getReversals() {
-         return AlternativeLoci.getAll()
-    }
-
 }
