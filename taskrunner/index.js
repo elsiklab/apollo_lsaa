c779e90be8e8c296dd9c5db0cbf248e9d2cb1cef
Colin <colin.diesh@gmail.com>
2016-07-13 11:24:32 -0500

    Add config.json

--- taskrunner/index.js
+++ taskrunner/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
+var exec = require('child_process').exec;
+var config = require('./config.json');
+
 var app = express();
 
-
-
+// allow CORS
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
+// retrieve results
 app.get('/', function(req, res) {
-    res.send('hello world');
+    exec(`scaffolder sequence ${config.yamlfile} ${config.fastafile}`, function(error, stdout, stderr) {
+        res.setHeader('Content-disposition', 'attachment; filename=result.fasta');
+        res.contentType('application/octet-stream');
+        res.send(stdout);
+    });
 });
 
 app.listen(process.env.EXPRESS_PORT || 4730);
