6953a6fd38dde01a6e29c623cd061c149bdd7b12
Colin <colin.diesh@gmail.com>
2016-08-15 14:43:37 -0500

    Fixup dataset

--- grails-app/services/org/elsiklab/EditScaffoldsService.groovy
+++ grails-app/services/org/elsiklab/EditScaffoldsService.groovy
@@ -15,40 +15,42 @@ class EditScaffoldsService {
 
     def getTransformations(def organism) {
         def map = []
-        def prevstart = 1 
+        def prevstart = 1
         def fastaFile = FastaFile.findByOrganism(organism)
         def list = this.getAltLoci(organism)
 
         for(int i = 0; i < list.size(); i++) {
             AlternativeLoci curr = list[i];
-            AlternativeLoci next = list[i + 1]; 
-            map << [
-                sequence: [
-                    source: curr.featureLocation.sequence.name,
-                    start: prevstart,
-                    stop: curr.featureLocation.fmin - 1,
-                    filename: fastaFile.filename,
-                    reverse: false
-                ]   
-            ]   
-            map << [
-                sequence: [
-                    source: curr.name_file,
-                    start: curr.featureLocation.fmin,
-                    stop: curr.featureLocation.fmax,
-                    reverse: curr.reversed ?: false,
-                    filename: curr.fasta_file.filename
-                ]   
-            ]   
-            map << [
-                sequence: [
-                    source: curr.featureLocation.sequence.name,
-                    start: curr.featureLocation.fmax + 1,
-                    stop: (i == list.size() - 1) ? (curr.featureLocation.sequence.length) - 1 : next.featureLocation.fmin,
-                    filename: fastaFile.filename,
-                    reverse: false
-                ]   
-            ]   
+            AlternativeLoci next = list[i + 1];
+            if(curr.reversed) {
+                map << [
+                    sequence: [
+                        source: curr.featureLocation.sequence.name,
+                        start: prevstart,
+                        stop: curr.featureLocation.fmin - 1,
+                        filename: fastaFile.filename,
+                        reverse: false
+                    ]
+                ]
+                map << [
+                    sequence: [
+                        source: curr.name_file,
+                        start: curr.featureLocation.fmin,
+                        stop: curr.featureLocation.fmax,
+                        reverse: curr.reversed ?: false,
+                        filename: curr.fasta_file.filename
+                    ]
+                ]
+                map << [
+                    sequence: [
+                        source: curr.featureLocation.sequence.name,
+                        start: curr.featureLocation.fmax + 1,
+                        stop: (i == list.size() - 1) ? (curr.featureLocation.sequence.length) - 1 : next.featureLocation.fmin,
+                        filename: fastaFile.filename,
+                        reverse: false
+                    ]
+                ]
+            }
             prevstart = curr.featureLocation.fmin
         }
         return map
--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -5,7 +5,10 @@ git clone --depth 1 https://github.com/gmod/jbrowse
 cd jbrowse
 bower install
 cd ../../
+rm -rf web-app/jbrowse/data
+rm -rf web-app/jbrowse/data_transformed
 cp -R test/integration/data/pyu_data web-app/jbrowse/data
+cp -R test/integration/data/pyu_data_lsaa web-app/jbrowse/data_transformed
 rm -rf web-app/jbrowse/plugins/LSAA
 rm -rf web-app/jbrowse/plugins/PairedReadViewer
 cp -R client/LSAA web-app/jbrowse/plugins/LSAA
